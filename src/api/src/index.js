2192299b58c92a4cb03498b5b298c33f3d963dd5
aldairandre <aldairandre99@gmail.com>
2022-12-04 05:22:08 +0000

    ➕ Adding Morgan

--- src/api/src/index.js
+++ src/api/src/index.js
@@ -1,7 +1,25 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 const books = require('./routes/books')
 
+app.use(morgan('dev'))
+
 app.use('/books',books)
+
+app.use((req,res,next)=>{
+  const error = new Error('Nothing Here')
+  error.status = 404
+  next(error)
+})
+
+app.use((error,req,res,next)=>{
+  res.status(error.status || 500)
+  return res.send({
+    erro : {
+      message : error.message
+    }
+  })
+})
 
 app.listen(9001,'localhost',() => console.log('Listening on port http://localhost:9001 🚀'))
